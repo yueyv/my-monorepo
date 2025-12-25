a5d9c9a862be76b7ecd44d1eb28ea768735c8e1a
刘辉 <3274927818@qq.com>
2025-12-25 16:42:27 +0800

    更新

---
+++ apps/nuxt-demo/app/pages/ui/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Animate</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'UI 效果',
+  icon: 'Menu',
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+  },
+});
+</script>
---
+++ apps/nuxt-demo/app/pages/ui/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'Test',
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+  },
+});
+</script>
